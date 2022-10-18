e769c75d0a2f6fed9facd569717009668e746fbb
Exllated <musicmany222@gmail.com>
2022-10-18 11:09:54 +0200

    fix #5: Text field not showing empty lines

--- formatting.py
+++ formatting.py
@@ -2,5 +2,8 @@
 def ide_text_list_to_html(text_list):
     html = ''
     for i, line in enumerate(text_list):
-        html += f'<p>{line}</p>'
+        if line == '':
+            html += '<br>'
+        else:
+            html += f'<p>{line}</p>'
     return html
