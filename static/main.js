2d888b456f2986df2443074ff899485a7e080b7e
Exllated <musicmany222@gmail.com>
2022-10-18 11:32:31 +0200

    fix #7: Rewrite all URLs without 'viki' prefix

--- static/main.js
+++ static/main.js
@@ -11,7 +11,7 @@ async function dockeydown(e) {
     }
 
     e.preventDefault();
-    jsonResp = JSON.parse(await fetchAsync('/viki/api/on_key_pressed?k=' + e.key + '&alt=' + (e.altKey ? '1' : '0') + '&ctrl=' + (e.ctrlKey ? '1' : '0')));
+    jsonResp = JSON.parse(await fetchAsync('/api/on_key_pressed?k=' + e.key + '&alt=' + (e.altKey ? '1' : '0') + '&ctrl=' + (e.ctrlKey ? '1' : '0')));
     richText.innerHTML = jsonResp.html;
     primaryCaret.style = 'top: ' + letter_height * jsonResp.caret_position[0] + 'px; left: ' + letter_width * jsonResp.caret_position[1] + 'px;';
 
